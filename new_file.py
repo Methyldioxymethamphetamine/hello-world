d0a687696158eb28f82089501b29f657436bcd06
dushyant <dushyant7563@gmail.com>
2025-07-03 19:34:41 +0530

    Even more testing

--- new_file.py
+++ new_file.py
@@ -1,1 +1,18 @@
-print("Even more testing lol")
+input = "test.txt"
+output = "output.txt"
+paragraph = ""
+i = 1
+with open(input) as f:
+    for line in f:
+
+        if "Dushyant" in line:
+            print(f"\n\n My Name is in line {i} \n\n")
+        paragraph += f"{line.strip()} \n"
+
+        i += 1
+
+
+print(paragraph)
+with open(output, "w") as f:
+    f.write(paragraph)
+

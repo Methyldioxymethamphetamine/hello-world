a6d14214dfc3750160c25649451c76d2f6e4a16e
dushyant <dushyant7563@gmail.com>
2025-07-04 22:05:21 +0530

    Updated Script for Ubuntu Mouse Scrolling

--- new_file.py
+++ new_file.py
@@ -1,18 +1,7 @@
-input = "test.txt"
-output = "output.txt"
-paragraph = ""
-i = 1
-with open(input) as f:
-    for line in f:
-
-        if "Dushyant" in line:
-            print(f"\n\n My Name is in line {i} \n\n")
-        paragraph += f"{line.strip()} \n"
-
-        i += 1
-
-
-print(paragraph)
-with open(output, "w") as f:
-    f.write(paragraph)
-
+import csv 
+file = "data.csv"
+with open(file) as f:
+    reader = csv.reader(f)
+    header = next(reader)
+    for index,column in enumerate(header):
+        print(f"Column {index}: {column}")
